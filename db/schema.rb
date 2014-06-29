2f90c7f4d3272acc27038f8686d833757b57fb02
takatsuna <Takatsuna.Yuusuke@icloud.com>
2014-06-30 00:52:07 +0900

    customize comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627130923) do
+ActiveRecord::Schema.define(version: 20140629152205) do
+
+  create_table "comments", force: true do |t|
+    t.string   "reviewer"
+    t.string   "reviewcomment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "idea_id"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
